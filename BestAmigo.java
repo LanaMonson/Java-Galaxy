50ba6a8776f5fc6e641cab9134f3e7fc20a739a8
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-13 20:21:18 -0600

    Create BestAmigo.java

---
+++ BestAmigo.java
@@ -0,0 +1,13 @@
+public class Home
+{
+    public static void main(String[] args)
+    {
+        /*
+        'Amigo Is The Best'
+        */
+        System.out.print("Amigo ");
+        System.out.print("Is ");
+        System.out.print("The ");
+        System.out.print("Best");
+    }
+}
