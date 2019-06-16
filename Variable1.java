5330f8e91eec34705d418a8de1346dbad835d4fe
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-16 08:43:06 -0600

    Update Variable1.java

--- Variable1.java
+++ Variable1.java
@@ -18,3 +18,10 @@ public class Solution {
 }
 
 
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        System.out.println(number * number);
+    }
+}
