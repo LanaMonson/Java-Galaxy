f609e711db8878227614223c3e9312d566ca28df
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-17 06:57:12 -0600

    Create HelloAmigo.java

---
+++ HelloAmigo.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Amigo";
+        String text = "Hello, " + name + "!";
+        System.out.println(text);
+    }
+}
