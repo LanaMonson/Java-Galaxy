43339333ea021bf4940b62a020adfe932e1cea2e
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-17 07:06:40 -0600

    Create AgeFifteen.java

---
+++ AgeFifteen.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Amigo";
+        int age = 15;
+        int money = 500;
+        int weight = 70;
+        System.out.println(age);
+    }
+}
+
+//OUTPUT:
+//15
