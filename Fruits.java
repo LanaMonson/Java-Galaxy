101fa93bc279db179f68c3064940f291a54263a8
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-17 19:15:06 -0600

    Create Fruits.java

---
+++ Fruits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static void main(String[] args) {
+        String apple = "Яблоко";
+        String porridge = "Каша";
+        String sausage = "Колбаса";
+        String peach = "Персик";
+
+        System.out.println(apple);
+        System.out.println(peach);
+    }
+}
+
