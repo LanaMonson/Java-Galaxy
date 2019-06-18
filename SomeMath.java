5d6e985c9a8c2d9804a61dc51d0b9f54237888bb
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-17 18:43:15 -0600

    Create SomeMath.java

---
+++ SomeMath.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 30;
+        int c = 14;
+        System.out.print(a + b + c);
+    }
+}
+
+//OUTPUT:
+//100
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        double c = b + 38;
+        //int d = a + 12;
+        //double e = 12.3;
+        String s = "s" + a;
+        String s1 = a + "b";
+        //String s2 = "a";
+        String s3 = s1 + "a";
+        String s4 = s3 + "b";
+        System.out.println(c + s4 + s);
+    }
+}
+//OUTPUT:
+//53.010babs10
+
