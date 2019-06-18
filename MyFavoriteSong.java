54249b98e76e154556c44d4983a6f81f80127773
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-17 19:22:05 -0600

    Create MyFavoriteSong.java

---
+++ MyFavoriteSong.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мy favorite song:");
+        System.out.println("");
+        System.out.println("The sea knows where are the rocks");
+        System.out.println("And drowning is no sin");
+        System.out.println("You know where my heart is");
+        System.out.println("The same place that yours has been");
+        System.out.println("And we know that we fear to win");
+        System.out.println("And so we end before we begin");
+        System.out.println("Before we begin");
+    }
+}
