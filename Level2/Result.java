431671742400b291dc22e5d70cc77b863e277822
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-20 20:42:22 -0600

    Create Result.java

---
+++ Level2/Result.java
@@ -0,0 +1,17 @@
+public class FileCopy
+{
+    public static void main(String[] args) throws IOException
+    {
+        FileInputStream  fileInputStream = new FileInputStream("c:\data.txt");
+        FileOutputStream  fileOutputStream = new FileOutputStream("c:\result.txt");
+
+        while (fileInputStream.available() > 0)
+        {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
