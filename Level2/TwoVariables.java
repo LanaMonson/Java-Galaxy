5298082249e13bc14c1a19c154a7ea5612739e03
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-22 12:40:31 -0600

    Create TwoVariables.java

---
+++ Level2/TwoVariables.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 5;
+        int b = 7;
+        System.out.println(a+b);
+        System.out.println(a*b);
+    }
+}
+
+//OUTPUT:
+//12
+//35
