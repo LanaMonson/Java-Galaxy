1e9454f1cf9d1c7d6e58837c3f94168532ed1546
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-22 12:18:13 -0600

    Create NewSalary.java

---
+++ Level2/NewSalary.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) {
+        hackSalary(70000);
+    }
+
+    public static void hackSalary(int a) {
+        System.out.println("Your new salary is: "+ (a + =10000) +" dollars.");
+    }
+}
