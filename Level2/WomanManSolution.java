f37781d16a2564203283c60915adcd95a10d4297
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-23 10:48:31 -0600

    Create WomanManSolution.java

---
+++ Level2/WomanManSolution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Man man = new Man();
+        Woman woman = new Woman();
+        woman.husband = man;
+        man.wife = woman;
+    }
+
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+}
