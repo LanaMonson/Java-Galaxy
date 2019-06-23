21e7a18606f19837ede192b196283495f5136a5f
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-23 11:03:19 -0600

    Create NewCat.java

---
+++ Level2/NewCat.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat barsik = new Cat();
+        
+        Cat murka = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
+
