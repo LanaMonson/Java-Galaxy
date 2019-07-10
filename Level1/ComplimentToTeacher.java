2b80f218e1fd7f51550b76eb7b8f9c4be8021b2d
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-09 20:20:43 -0600

    Update ComplimentToTeacher.java

--- Level1/ComplimentToTeacher.java
+++ Level1/ComplimentToTeacher.java
@@ -1,9 +1,9 @@
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Элли самая красивая");
-        System.out.println("Элли самая красивая");
-        System.out.println("Элли самая красивая");
-        System.out.println("Элли самая красивая");
-        System.out.println("Элли самая красивая");
+        System.out.println("Ellie is so beautiful");
+        System.out.println("Ellie is so beautiful");
+        System.out.println("Ellie is so beautiful");
+        System.out.println("Ellie is so beautiful");
+        System.out.println("Ellie is so beautiful");
     }
 }
