4d7d50ea987d4c4e7bd03080b0b9d8b20d504efd
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-09 20:17:31 -0600

    Update Fruits.java

--- Level1/Fruits.java
+++ Level1/Fruits.java
@@ -1,9 +1,9 @@
 public class Solution {
     public static void main(String[] args) {
-        String apple = "Яблоко";
-        String porridge = "Каша";
-        String sausage = "Колбаса";
-        String peach = "Персик";
+        String apple = "Apple";
+        String porridge = "Cereal";
+        String sausage = "Hot dog";
+        String peach = "Peach";
 
         System.out.println(apple);
         System.out.println(peach);
