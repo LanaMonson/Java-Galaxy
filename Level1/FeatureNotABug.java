bb48f8c5a6e592dd305d00b218f3905426a79a11
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-09 20:15:11 -0600

    Update FeatureNotABug.java

--- Level1/FeatureNotABug.java
+++ Level1/FeatureNotABug.java
@@ -1,5 +1,5 @@
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Это не баг, это фича.");
+        System.out.println("This is not a bug, this is a feature.");
     }
 }
